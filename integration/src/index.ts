a4c54fb4115fd962d4453b315c05c7e1a34413e5
adedolapo oduguwa <adedolapoo@gmail.com>
2022-08-18 14:26:33 +0100

    initial commit

---
+++ integration/src/index.ts
@@ -0,0 +1,5 @@
+import { Logger } from "@adedolapoo-org/logger";
+import { Config } from "@adedolapoo-org/config";
+
+console.log(new Logger());
+console.log(new Config());
---
+++ packages/config/src/config.ts
@@ -0,0 +1,5 @@
+export class Config {
+    get(id) {
+        return {};
+    }
+}
